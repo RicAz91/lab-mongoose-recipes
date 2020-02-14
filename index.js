98ef3c5dbd1fc91c1d7582a40646648fb6e813e9
Ric Az <ricardoazevedo96@gmail.com>
2020-02-14 12:12:28 +0000

    done

--- index.js
+++ index.js
@@ -11,9 +11,50 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+  .then(self => {
+    return self.connection.dropDatabase();
+  })
   .then(() => {
-    console.log(`Connected to the database: "${x.connections[0].name}"`);
+    console.log(`Connected to the database."`);
+    return Recipe.create({
+      title: 'Frango à lá Ricardo',
+      level: 'Easy Peasy',
+      ingredients: ['chiken meat', 'rice', 'shoyo', 'garlic', 'onion', 'cheese', 'salt', 'peper'],
+      cuisine: 'my own',
+      dishType: 'Dish',
+      duration: 20,
+      creator: 'Ricardo'
+    });
     // Run your code here, after you have insured that the connection was made
+  })
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+
+  .then(() => {
+    return Recipe.find({}, 'title');
+  })
+
+  .then(rec => {
+    console.log(rec);
+  })
+  .then(() => {
+    return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+  })
+  .then(() => {
+    console.log('time updated');
+  })
+  .then(() => {
+    Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(() => {
+    console.log('Deleted');
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('mongoose disconected');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
